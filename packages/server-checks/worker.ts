e58dbf89bc0e02e3a8fbce67368c83094c6c6958
Iris System <iris@iris.ac.nz>
2024-10-06 00:43:27 +0000

    give server checks a half-interval buffer for outdatedness

--- packages/server-checks/worker.ts
+++ packages/server-checks/worker.ts
@@ -16,7 +16,7 @@ async function processMachine(env, m) {
 
 	if (isNaN(mdata.ts)) {
 		out.push("- could not find last check timestamp");
-	} else if (mdata.ts < (Date.now() / 1000 - 60)) {
+	} else if (mdata.ts < (Date.now() / 1000 - 90)) {
 		out.push(`- last check is too old (${mdata.ts - Date.now() / 1000} seconds ago)`);
 	}
 
