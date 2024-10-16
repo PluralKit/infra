ce5ba67e1bbb8a934657171d00e9065a2bc50477
alyssa <alyssa@mailc.net>
2024-10-16 09:08:02 +0900

    server-checks silence/override

--- packages/server-checks/worker.ts
+++ packages/server-checks/worker.ts
@@ -2,13 +2,21 @@ const notify = (env, msg) => fetch(env.webhookUrl+"?wait=true", { method: "POST"
 
 const machinesToCheck = [ "compute03", "db2", "vps" ];
 
-async function processMachine(env, m) {
+async function processMachine(env, m, silences, notifRole) {
 	let mdata;
+
+	if (!!silences.find(x => x.checkPrefix == "*" && x.node == m )) {
+		console.log(`machine ${m} is silenced, skipping`);
+		return;
+	}
+
+	notifRole = notifRole ?? "<@&1291763746657669211>";
+
 	try {
 		mdata = await fetch(`http://${m}.pluralkit.net:19999/checks`);
 		mdata = await mdata.json();
 	} catch(e) {
-		await notify(env, `<@&1291763746657669211>, on ${m}: failed to query checks status: ${e}`);
+		await notify(env, `${notifRole}, on ${m}: failed to query checks status: ${e}`);
 		return;
 	}
 
@@ -23,10 +31,12 @@ async function processMachine(env, m) {
 	if (!Array.isArray(mdata.errors)) {
 		out.push("- could not find errors array");
 	} else if (mdata.errors.length > 0) {
-		out.unshift(...mdata.errors.map(e => `- ${e}`));
+		let errors = mdata.errors.filter(e => !silences.find(x => (x.node == m || x.node == "*") && (e == x.check || e.startsWith(x.checkPrefix))));
+		if (errors.length != mdata.errors.length) console.log("some errors silenced on "+m);
+		out.unshift(...errors.map(e => `- ${e}`));
 	}
 
-if (out.length > 0) await notify(env, `<@&1291763746657669211>, on ${m}:\n${out.join("\n")} \n`);
+	if (out.length > 0) await notify(env, `${notifRole}, on ${m}:\n${out.join("\n")} \n`);
 	else console.log(`${m}: all checks ok!`);
 }
 
@@ -35,7 +45,21 @@ export default {
 // test
 //		console.log(JSON.stringify(await notify(env, "test").then(x => x.json())));
 
-		await Promise.all(machinesToCheck.map(m => processMachine(env, m)))
+		let silences = await env.kv.get("silences").then(x => JSON.parse(x));
+//			JSON.parse(x).map(x => ({ check: x.split("@")[0], node: x.split("@")[1] }))
+//					.map(x => {
+//						if (x.check.endsWith("*")) {
+//							x.checkPrefix = x.check[x.check.length - 1];
+//							x.check == null;
+//						}
+//						return x;
+//					})
+//			);
+
+		let notifRole = await env.kv.get("notif");
+		if (notifRole == "") notifRole = null;
+
+		await Promise.all(machinesToCheck.map(m => processMachine(env, m, silences, notifRole)))
 
 		console.log("cron processed!");
 	},
