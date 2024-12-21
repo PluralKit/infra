e8f5fdddbfb63c462a97dfb2210e1b94ebc3475c
alyssa <alyssa@mailc.net>
2024-12-22 04:17:42 +0900

    fix dns/creds.sh (1password pls)

--- dns/creds.sh
+++ dns/creds.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo '{"cloudflare":{"TYPE":"CLOUDFLAREAPI","accountid":"'"$(op item get --vault=pluralkit --field=account_id cloudflare)"'","apitoken":"'"$(op item get --vault=pluralkit --field=dnscontrol_auth_token cloudflare)"'"}}'
+echo '{"cloudflare":{"TYPE":"CLOUDFLAREAPI","accountid":"'"$(op item get --vault=pluralkit --field=account_id cloudflare)"'","apitoken":"'"$(op item get --vault=pluralkit --field=dnscontrol_auth_token cloudflare --reveal)"'"}}'
