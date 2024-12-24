2171da4a1c21adb6e739b0ee88d45c02e7c33e65
alyssa <alyssa@mailc.net>
2024-12-24 07:05:37 +0000

    optionally run rebuilds in parallel

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,30 @@
 #!/usr/bin/env bash
 
+export NIX_SSHOPTS="-i /root/.ssh/provisioner"
+
+if [ "$1" == "do_rebuild" ]; then
+  user=${user:-root}
+
+  hostname=$3
+  action=$2
+
+  exec > >(trap "" INT TERM; sed "s/^/$hostname(stdout): /")
+  exec 2> >(trap "" INT TERM; sed "s/^/$hostname(stderr): /" | grep -v 'evaluating file' >&2)
+
+  ip=$(nix eval -v -L ".#nixosConfigurations.$hostname.config.systemd.network.networks.eth0.address" | jq -r .[0] | sed 's/\// /' | awk '{print $1}')
+  echo $hostname at $ip
+  if [ "$wait" == "true" ]; then
+    echo "tap enter to run build"
+    read
+  fi
+  nixos-rebuild $action --flake .#$hostname --target-host $user@$ip --build-host $user@$ip --use-remote-sudo
+  exit 0
+fi
+
 query=$1
-user=${user:-alyssa}
-wait=${wait:-false}
+action=$2
+
+export wait=${wait:-false}
 
 hosts=($(find hosts -type f -exec basename {} \; | grep -v default | sed 's/.nix//'))
 
@@ -22,13 +44,12 @@ echo "rebuilding ${hosts[@]}"
 echo "enter to confirm"
 read
 
-for hostname in ${hosts[@]}; do
-  ip=$(nix eval -v -L ".#nixosConfigurations.$hostname.config.systemd.network.networks.eth0.address" | jq -r .[0] | sed 's/\// /' | awk '{print $1}')
-  echo $hostname at $ip
-  if [ "$wait" == "true" ]; then
-    echo "tap enter to run build"
-    read
-  fi
-  nixos-rebuild $2 --flake .#$hostname --target-host $user@$ip --build-host $user@$ip --use-remote-sudo
-done
-
+if [ "$parallel" == "true" ]; then
+  # shut up
+  echo 'will cite' | nix run nixpkgs#parallel -- --citation >/dev/null 2>/dev/null
+  nix run nixpkgs#parallel -- --ungroup "$0 do_rebuild $action {}" ::: ${hosts[@]}
+else
+  for hostname in ${hosts[@]}; do
+    $0 do_rebuild $action $hostname
+  done
+fi
