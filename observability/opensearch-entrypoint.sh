7adaa3883e21c08a114493b9bd705da2cdc4670c
alyssa <alyssa@mailc.net>
2025-01-18 17:25:07 +0000

    move observability to own repo

--- observability/opensearch-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sysctl vm.max_map_count=1048576
-sysctl fs.file-max=65536
-ulimit -n 65535
-ulimit -l unlimited
-chown -R opensearch /usr/share/opensearch/data
-exec su -p opensearch ./opensearch-docker-entrypoint.sh
